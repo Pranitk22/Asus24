e53c72b65d04e16c71e58e49f17ef70cdd10a3fe
7pranit <pranitkonde007@gmail.com>
2024-01-31 05:31:39 +0530

    My own commit to push

---
+++ src/com/addas/Firstcom.java
@@ -0,0 +1,15 @@
+package com.addas;
+
+public class Firstcom {
+
+	public static void main(String[] args) {
+		
+		int a = 20;
+		int b = 50;
+		
+		int div = 50/20;
+		
+		System.out.println("Division is: "+div);
+				
+	}
+}
